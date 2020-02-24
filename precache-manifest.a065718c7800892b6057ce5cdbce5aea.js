dcd7df38119310209eeb12fbf14c56d749e9b080
alifm22 <alimoghadam.fm.33@gmail.com>
2020-02-25 02:08:12 +0330

    Updates

---
+++ precache-manifest.a065718c7800892b6057ce5cdbce5aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04e3453eafb1d1399d4d33bef976fd8",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "f03ce55824a4ea5b059a",
+    "url": "/todo/static/css/main.7d60f9ac.chunk.css"
+  },
+  {
+    "revision": "8d469ac2f98cd0fe9acb",
+    "url": "/todo/static/js/2.bae2488b.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/todo/static/js/2.bae2488b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03ce55824a4ea5b059a",
+    "url": "/todo/static/js/main.0d03548f.chunk.js"
+  },
+  {
+    "revision": "dd9adfdb09f83cb1a7aa",
+    "url": "/todo/static/js/runtime-main.f7a45fe9.js"
+  }
+]);
